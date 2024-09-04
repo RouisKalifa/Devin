dda70d178481ee7e51e9f9619aa9b61b76143ff7
Kalifa <kalifarouis@leschassagnes.net>
2024-09-04 23:27:26 +0200

    Implémentation de la fonction SaisieNombre

--- tp1-devin/tp1-devin/Program.cs
+++ tp1-devin/tp1-devin/Program.cs
@@ -9,24 +9,34 @@ namespace tp1devin
 {
     internal class Program
     {
+        static int SaisieNombre()
+        {
+            int nb;
+            while (!Int32.TryParse(Console.ReadLine(), out nb))
+            {
+                Console.WriteLine("vous devez entrer un nombre entier");
+            }
+            return nb;
+        }
         static void Main(string[] args)
         {
             //variable locales
 
             Int32 nbu, nbo,nbEssai ;
-            bool success;
+            //bool success;
             Random nbAlea = new Random();
             nbo = nbAlea.Next(0, 10);
             Console.WriteLine("Au joueur de choisir son nombre entre 0 et 10");
-            success = Int32.TryParse(Console.ReadLine(), out nbu );
-            while(!success)
-            {
-                Console.WriteLine("vous devez entrer un nombre entier"); 
-                success = Int32.TryParse(Console.ReadLine(),out nbu);
-            }
+            //success = Int32.TryParse(Console.ReadLine(), out nbu );
+            //while(!success)
+            //{
+                //Console.WriteLine("vous devez entrer un nombre entier"); 
+                //success = Int32.TryParse(Console.ReadLine(),out nbu);
+            //}
             //nbu = Int32.Parse(Console.ReadLine());
             
-            nbEssai = 1; 
+            nbEssai = 1;
+            nbu = SaisieNombre();
 
             //Tant que le nombre de la machine n'est pas decouvert
 
@@ -41,12 +51,13 @@ namespace tp1devin
                     Console.WriteLine("trop grand");
                 }
                 Console.WriteLine("Au joueur de choisir son nombre entre 0 et 10");
-                success = Int32.TryParse(Console.ReadLine(), out nbu);
-                while (!success)
-                {
-                    Console.WriteLine("Vous devez entrer un nombre entier");
-                    success = Int32.TryParse(Console.ReadLine(), out nbu);
-                }
+                //success = Int32.TryParse(Console.ReadLine(), out nbu);
+               // while (!success)
+                //{
+                   // Console.WriteLine("Vous devez entrer un nombre entier");
+                    //success = Int32.TryParse(Console.ReadLine(), out nbu);
+                //}
+                nbu = SaisieNombre();
                 nbEssai++;
             }
             if (nbo == nbu)
